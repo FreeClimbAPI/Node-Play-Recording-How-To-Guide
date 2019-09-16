44d4ddc362c2c48abd469df97a6869fcb5c08d8d
Francis San Filippo <fsanfilipp@vailsys.com>
2019-09-16 15:24:31 -0500

    initial commit

---
+++ playARecording.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const persephonySDK = require('@persephony/sdk')
+
+const app = express()
+app.use(bodyParser.json())
+// Where your app is hosted ex. www.myapp.com
+const port = process.env.PORT || 3000
+// your Persephony API key (available in the Dashboard) - be sure to set up environment variables to store these values
+const accountId = process.env.accountId
+const authToken = process.env.authToken
+const persephony = persephonySDK(accountId, authToken)
+const applicationId = process.env.applicationId
+
+// Invoke create method to initiate the asynchronous outdial request
+persephony.api.calls.create(to, from, applicationId).catch(err => {/* Handle Errors */ })
+
+// Handles incoming calls. Set with 'Call Connect URL' in App Config
+app.post('/incomingCall', (req, res) => {
+  const play = persephony.percl.play(recordingUrl)
+  const percl = persephony.percl.build(play)
+  res.status(200).json(percl)
+})
+
+// Specify this route with 'Status Callback URL' in App Config
+app.post('/status', (req, res) => {
+  // handle status changes
+  res.status(200)
+})
+
+app.listen(port, () => {
+  console.log(`Started server on port ${port}`)
+})
