cb086d148c23f277115057009f6325b0c52d8166
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-28 11:03:17 -0500

    freeclimb code changes

--- playARecording.js
+++ playARecording.js
@@ -1,25 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const persephonySDK = require('@persephony/sdk')
+const freeclimbSDK = require('@freeclimb/sdk')
 
 const app = express()
 app.use(bodyParser.json())
 // Where your app is hosted ex. www.myapp.com
 const port = process.env.PORT || 3000
-// your Persephony API key (available in the Dashboard) - be sure to set up environment variables to store these values
+// your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-const persephony = persephonySDK(accountId, authToken)
+const freeclimb = freeclimbSDK(accountId, authToken)
 const applicationId = process.env.APPLICATION_ID
 
 // Invoke create method to initiate the asynchronous outdial request
-persephony.api.calls.create(to, from, applicationId).catch(err => {/* Handle Errors */ })
+freeclimb.api.calls.create(to, from, applicationId).catch(err => {/* Handle Errors */ })
 
 // Handles incoming calls. Set with 'Call Connect URL' in App Config
 app.post('/incomingCall', (req, res) => {
-  const play = persephony.percl.play(recordingUrl)
-  const percl = persephony.percl.build(play)
+  const play = freeclimb.percl.play(recordingUrl)
+  const percl = freeclimb.percl.build(play)
   res.status(200).json(percl)
 })
 
